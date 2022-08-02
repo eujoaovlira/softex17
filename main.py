fe8c1d8033678e0480a1a5195a16c2108e011ed0
João Victor Lira Victor <109807072+eujoaovlira@users.noreply.github.com>
2022-08-02 12:00:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+#codigos de atribuição
+
+quantrodas = 4 
+peso = 6000
+quantpessoas = 8
+
+if quantrodas <=3:
+ print ('Habilitação A')
+
+elif quantrodas == 4: peso <= 3500
+elif quantpessoas <8:
+  print ('Habilitacão B')
+
+elif quantrodas >= 4: peso <=6000
+print ('Se seu veicúlo tem entre 3500 e 6000 sua Habilitação é a C')
+
+
+if quantrodas >=4:
+  if peso <= 6000:
+    print ('Habilitacão C')
+
+  elif quantrodas >= 4:
+    if quantpessoas > 8:
+      print ('Habiitação D')
+
+    elif quantrodas >= 4:
+      if peso > 6000:
+        print ('Habilitação E')
+
+
+        
+    
+    
+  
+  
